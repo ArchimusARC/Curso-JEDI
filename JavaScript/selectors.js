f9bd9e517f0e0f0b4c8297ab086bac529ac6290b
ArchimusARC <ruiza3e@gmail.com>
2018-02-06 12:57:16 +0100

    selectors actualizado. Canvas pelota

---
+++ JavaScript/selectors.js
@@ -0,0 +1,15 @@
+window.onload() {
+
+	function onClickRed() {
+		console.log("Red");
+	}
+
+	function onClickBlue() {
+		console.log("Blue");
+	}
+
+	function deleteThis() {
+		console.log("DeleteThis");
+	}
+}
+
