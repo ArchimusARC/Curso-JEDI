addb61b6d11da5cdc7caa7869a2e3e476f2c7627
ArchimusARC <ruiza3e@gmail.com>
2018-02-06 22:40:11 +0100

    Sigo trabajando en selectors

--- JavaScript/selectors.js
+++ JavaScript/selectors.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-
-	document.getElementById('button1').onClick = function() {
+	
+	document.getElementById("red_button").onClick = function() {
 		console.log("Red");
 	}
 
-	document.getElementById('button2').onClick = function() {
+	document.getElementById("blue_button").onClick = function() {
 		console.log("Blue");
 	}
 
-	document.getElementById('deleteThis').onClick = function() {
+	document.getElementById("deleteThis").onClick = function() {
 		console.log("DeleteThis");
 	}
 }
