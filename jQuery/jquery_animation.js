ab8530a67ae5352061a29dfed98c58cddb39ad31
Adrián Ruiz Cordón <ruiza3e@gmail.com>
2018-02-07 12:06:55 +0100

    Animation exercice: incompleto

--- jQuery/jquery_animation.js
+++ jQuery/jquery_animation.js
@@ -1,10 +1,39 @@
 function wololo() {
+	$("#holo").css({
+		transform:"rotateY(180deg)",
+		transition: "5s",
+	});
+	$("#holo").css("background-image", "url(salty.jpg)");
+}
+
+function expando() {
 	$("#holo").animate({
-		transform: "rotate(180deg)"
-	}, 1500);
+		width: "634px",
+		height:"634px"
+	}, 5000);
+}
+
+function shrinku() {
+	$("#holo").animate({
+		width: "317px",
+		height:"317px"
+	}, 5000);
+}
+
+function giru_giru(){
+	
+}
+
+
+function fade_to_none() {
+	$("#holo").fadeTo(15000, 0.001);
 }
 
 
 $(window).on("load", function(){
-    wololo();
+ 	wololo();
+ 	expando();
+ 	shrinku();
+ 	//giru_giru();
+ 	fade_to_none();
 });
