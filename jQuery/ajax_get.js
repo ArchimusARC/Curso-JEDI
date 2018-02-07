a8e299135533626b5228c4133c4a4d37e44d8a0f
Adrián Ruiz Cordón <ruiza3e@gmail.com>
2018-02-07 20:05:20 +0100

    ajax pokemon table en proceso

---
+++ jQuery/ajax_get.js
@@ -0,0 +1,18 @@
+$(window).on("load", function(){
+
+	
+	function get_data(pkmn_id) {
+		var poke = $.get("https://www.pokeapi.co/api/v2/pokemon/"+pkmn_id+"/",
+			function(data) {
+
+
+			});
+	}
+
+	function show_result(pkmn) {
+
+	}
+
+	var current_pkmn = 549;
+	var pkmn_data = get_data(549);
+}
