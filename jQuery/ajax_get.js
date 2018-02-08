1fc0d8c1445e1b9f3215018ebc843914cd0f95a0
Pepe Grillo <ruiza3e@gmail.com>
2018-02-08 11:34:51 +0100

    Ya se ven los sprites

--- jQuery/ajax_get.js
+++ jQuery/ajax_get.js
@@ -2,16 +2,38 @@ $(window).on("load", function(){
 
 	
 	function get_data(pkmn_id) {
-		var poke = $.get("https://www.pokeapi.co/api/v2/pokemon/"+pkmn_id+"/",
+		$.get("https://www.pokeapi.co/api/v2/pokemon/"+pkmn_id+"/",
 			function(data) {
-				
-			});
+				console.log(data.name);
+				console.log(data.id);
+				console.log(data.types[0].type.name);
+
+				$("#ID").text = data.id;
+				$("#nsprite").attr("src", data.sprites.front_default);
+				$("#ssprite").attr("src", data.sprites.front_shiny);
+				$("#name").text = data.name;
+				$("#type").text = data.types[0].type.name;
+				alert("Todo cargado");
+			}, "json");
 	}
 
-	function show_result(pkmn) {
-
-	}
 
 	var current_pkmn = 549;
-	var pkmn_data = get_data(549);
-}
+	get_data(current_pkmn);
+	
+	$("#next_btn").on({
+		click: function (){
+			current_pkmn+=1;
+			get_data(current_pkmn);
+			console.log(current_pkmn);
+		}
+	});
+
+	$("#last_btn").on({
+		click: function (){
+			current_pkmn-=1;
+			get_data(current_pkmn);
+			console.log(current_pkmn);
+		}
+	});
+})
