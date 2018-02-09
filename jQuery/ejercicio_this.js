e56159774830d89619f82b4122f138d05fe664df
ArchimusARC <ruiza3e@gmail.com>
2018-02-09 11:47:12 +0100

    Cambios leves

--- jQuery/ejercicio_this.js
+++ jQuery/ejercicio_this.js
@@ -1,9 +1,9 @@
 $(window).on("load", function(){
-	var texto = "";
+	var texto = $("#inguion").val();
 
 	function insert_text(text) {
-		this.texto = this.texto+text+"-";
-		$("#texto").text(this.texto);
+		this.texto = this.texto+"-";
+		$("#inguion").val(this.texto);
 	}
 
 	$("#inguion").keypress(function() {
